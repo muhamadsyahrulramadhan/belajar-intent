d9251988e7d375a6eeba15d971801a1343dca221
syahrul <syahrulramadhan.siswa@smkn4bdg.sch.id>
2020-03-27 11:45:55 +0700

    Commit pertama

---
+++ Intent/app/src/main/java/com/example/intent/Activity_Tentang.java
@@ -0,0 +1,14 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Tentang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__tentang);
+    }
+}
---
+++ Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void panggil(View view){
+        String nomor = "09667347";
+        Intent panggil = new Intent(Intent.ACTION_DIAL);
+        panggil.setData(Uri.fromParts("tel",nomor,null));
+        startActivity(panggil);
+    }
+    public void buka(View view) {
+        String url = "https://smkn4bdg.sch.id";
+        Intent buka = new Intent(Intent.ACTION_VIEW);
+        buka.setData(Uri.parse(url));
+        startActivity(buka);
+    }
+    public void tentang(View view){
+        Intent tentang = new Intent(MainActivity.this, Activity_Tentang.class);
+        startActivity(tentang);
+    }
+}
